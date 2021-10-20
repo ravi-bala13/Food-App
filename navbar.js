fb455077e0158afa2de23a623939174a6ebb2197
Bala Murugan R <bals161616@gmail.com>
2021-10-20 17:27:41 +0530

    food app files
    Assignment for food app given on 20.10.2021

---
+++ navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+
+    return `<div id="navbar">
+    <h3><a href="search.html">Home to search</a></h3>
+
+    <div id="options">
+      <h3><a href="today spcl.html">Today special</a></h3>
+      <h3><a href="get latest.html">Get latest receipe</a></h3>
+      <h3><a href="#">Sign In</a></h3>
+      <h3><a href="#">Sign Up</a></h3>
+    </div>
+  </div>`
+
+}
+
+export default navbar
